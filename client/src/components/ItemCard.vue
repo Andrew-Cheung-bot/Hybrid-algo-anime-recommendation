e5c318940691a4c138c5ba90c4ae7c67b992aaec
Potatomato <andrew.chu.cheung@gmail.com>
2024-04-23 12:45:51 +0800

    rename vue project

--- client/src/components/ItemCard.vue
+++ client/src/components/ItemCard.vue
@@ -1,7 +1,7 @@
 <template>
     <v-card variant="text" class="text" width="185px" color="#0B1622">
         <v-img style="width: 185px;height:265px" Default :src="props.Data.image_path"
-            lazy-src="/public/anime_images/doggie.jpg">
+            lazy-src="/anime_images/doggie.jpg">
             <template v-slot:placeholder>
                 <div class="d-flex align-center justify-center fill-height">
                     <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
